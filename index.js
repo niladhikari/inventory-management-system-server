c5fc3990d76fc6f7fe6d547183d272b2f1609578
niladhikari <niladhikari445@gmail.com>
2023-11-26 00:19:52 +0600

    1st of all set the server site...lot of thinks install...than create some collection for the data store...1st of all post the user data..than post the shop data..then create the jwt token system,,using the middlewere

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +11,91 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fcmyfrv.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    //db collection 
+    const userCollection = client.db("inventoryDB").collection("users");
+    const shopCollection = client.db("inventoryDB").collection("shops");
+
+
+        //jwt related api
+        app.post("/jwt", async (req, res) => {
+          const user = req.body;
+          const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "1h",
+          });
+          res.send({ token });
+        });
+    
+        //middleware
+        const verifyToken = (req, res, next) => {
+          console.log("inside verify token", req.headers.authorization);
+          if (!req.headers.authorization) {
+            return res.status(401).send({ message: "unauthorized access" });
+          }
+          const token = req.headers.authorization.split(" ")[1];
+          jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+              return res.status(401).send({ message: "unauthorized access" });
+            }
+            req.decoded = decoded;
+            next();
+          });
+        };
+    
+
+    //post method  add the user in the database
+    // insert email if user doesn't exists:
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      console.log(result);
+      res.send(result);
+    });
+
+     //Api for the shop collection
+    //post method for the shop
+    app.post("/shops", async (req, res) => {
+      const shop = req.body;
+      const result = await shopCollection.insertOne(shop);
+      console.log(result);
+      res.send(result);
+    });
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res) => {
